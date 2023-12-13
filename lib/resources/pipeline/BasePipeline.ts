538d8f6a2816fbd7104263957bb36c30a0299a02
Alexandro Isaac Aguilar Zarate <aleaguilar@palaceresorts.com>
2023-12-13 17:25:08 -0600

    added new stage

--- lib/resources/pipeline/BasePipeline.ts
+++ lib/resources/pipeline/BasePipeline.ts
@@ -1,7 +1,7 @@
 import { Construct } from 'constructs';
-import { SecretValue } from 'aws-cdk-lib';
-import { CodeStarConnectionsSourceAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { CodeBuildAction, CodeStarConnectionsSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Artifact, Pipeline, PipelineProps } from 'aws-cdk-lib/aws-codepipeline';
+import { PipelineProject } from 'aws-cdk-lib/aws-codebuild';
 
 export default class BasePipeline extends Pipeline {
   constructor(scope: Construct, id: string, props?: PipelineProps) {
@@ -9,6 +9,8 @@ export default class BasePipeline extends Pipeline {
   }
 
   execute() {
+    const sourceOutput = new Artifact();
+    const cdkOutput = new Artifact();
     // Agregar etapa de origen (Source)
     const sourceStage = this.addStage({
       stageName: 'Source',
@@ -18,12 +20,27 @@ export default class BasePipeline extends Pipeline {
     sourceStage.addAction(
       new CodeStarConnectionsSourceAction(
         {
-          output: new Artifact(),
+          output: sourceOutput,
           branch: 'main',
           owner: 'firepho92',
           repo: 'BasePipeline',
           actionName: 'GitHub_Source',
           connectionArn: 'arn:aws:codestar-connections:us-east-1:058632605534:connection/2d99428e-d740-40cb-9f88-ec8fd959dcf2'
+        }
+      )
+    );
+
+    const buildStage = this.addStage({
+      stageName: 'Build',
+    });
+
+    buildStage.addAction(
+      new CodeBuildAction(
+        {
+          actionName: 'CDK_Build',
+          project: new PipelineProject(this, 'CdkBuildProject'),
+          input: sourceOutput,
+          outputs: [cdkOutput],
         }
       )
     );
