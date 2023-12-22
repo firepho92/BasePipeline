ad2646fcbe48099a1f7a6ab0e95df0d39f72745d
Alexandro Isaac Aguilar Zarate <aleaguilar@palaceresorts.com>
2023-12-22 13:05:27 -0600

    updated to CDK standards for CDK pipelines deploy

--- lib/base_pipeline-stack.ts
+++ lib/base_pipeline-stack.ts
@@ -1,15 +1,29 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import BasePipeline from './resources/pipeline/BasePipeline';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BasePipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
+    const pipeline = new CodePipeline(this, `Main-pipeline`, {
+      pipelineName: `Main-pipeline`,
+      synth: new ShellStep('Synth', {
+          input: CodePipelineSource.connection(`firepho92/BasePipeline`,`dev`,{
+            connectionArn: 'arn:aws:codestar-connections:us-east-1:058632605534:connection/2d99428e-d740-40cb-9f88-ec8fd959dcf2',
+            actionName:'SourceGithub',
+            triggerOnPush: true
+          }),
+          commands: ['npm install']
+      })            
+  })
     // The code that defines your stack goes here
-    const basePipeline = new BasePipeline(this, 'MainPipeline', { pipelineName: 'MainPipeline' });
-    basePipeline.execute();
+    // const basePipeline = new BasePipeline(this, 'MainPipeline', { 
+    //   pipelineName: 'MainPipeline',
+    //   pipelineVersion: '1.0.0',
+    // });
+    // basePipeline.execute();
     // example resource
     // const queue = new sqs.Queue(this, 'BasePipelineQueue', {
     //   visibilityTimeout: Duration.seconds(300)
