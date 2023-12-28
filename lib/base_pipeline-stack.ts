71d2a80f19335b806d77dddbd383cb205dfbb767
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2023-12-27 21:22:10 -0600

    policy statement added

--- lib/base_pipeline-stack.ts
+++ lib/base_pipeline-stack.ts
@@ -1,19 +1,24 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import BasePipeline from './resources/pipeline/BasePipeline';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BasePipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     const pipeline = new CodePipeline(this, `Main-Pipeline`, {
+      synthCodeBuildDefaults: {
+        rolePolicy: [
+          new PolicyStatement()
+        ]
+      },
       pipelineName: `Moments-Pipeline`,
       synth: new ShellStep('Synth', {
           input: CodePipelineSource.connection(`firepho92/BasePipeline`,`dev`,{
             connectionArn: 'arn:aws:codestar-connections:us-east-1:058632605534:connection/2d99428e-d740-40cb-9f88-ec8fd959dcf2',
             actionName:'SourceGithub',
-            triggerOnPush: true
+            triggerOnPush: true,
           }),
           installCommands: [
             'node --version',
