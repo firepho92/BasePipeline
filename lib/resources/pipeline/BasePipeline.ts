69e281412944eadc3fb6e877e5c1aebbe3c09d38
Alexandro Isaac Aguilar Zarate <aleaguilar@palaceresorts.com>
2024-01-05 09:41:14 -0600

    updated to pipeline

--- lib/resources/pipeline/BasePipeline.ts
+++ lib/resources/pipeline/BasePipeline.ts
@@ -1,12 +1,13 @@
 import { Construct } from 'constructs';
 import { CodeBuildAction, CodeStarConnectionsSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
-import { Artifact } from 'aws-cdk-lib/aws-codepipeline';
+import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
 import { PipelineProject } from 'aws-cdk-lib/aws-codebuild';
 import { CodePipeline, CodePipelineProps } from 'aws-cdk-lib/pipelines';
 import { StageSynthesisOptions } from 'aws-cdk-lib';
 import { CloudAssembly, CloudAssemblyBuilder } from 'aws-cdk-lib/cx-api';
+import { Role } from 'aws-cdk-lib/aws-iam';
 
-export default class BasePipeline extends CodePipeline {
+export default class BasePipeline extends Pipeline {
   constructor(scope: Construct, id: string, props: CodePipelineProps) {
     super(scope, id, props);
   }
@@ -14,9 +15,11 @@ export default class BasePipeline extends CodePipeline {
   execute() {
     const sourceOutput = new Artifact();
     const cdkOutput = new Artifact();
+
+    const role = Role.fromRoleArn(this, 'CodeStarConnectionRole', 'arn:aws:iam::058632605534:role/CodeStarConnectionsRole');
     // Agregar etapa de origen (Source)
-    // const sourceStage = this.addStage({
-    //   stageName: 'Source',
+    const sourceStage = this.addStage({
+      stageName: 'Source',
     //   _assemblyBuilder: new CloudAssemblyBuilder,
     //   policyValidationBeta1: [],
     //   outdir: '',
@@ -27,35 +30,36 @@ export default class BasePipeline extends CodePipeline {
     //   },
     //   createBuilder: undefined,
     //   node: new Node
-    // });
+    });
     
     // Agregar acción de origen (Source action)
-    // sourceStage.addAction(
-    //   new CodeStarConnectionsSourceAction(
-    //     {
-    //       output: sourceOutput,
-    //       branch: 'main',
-    //       owner: 'firepho92',
-    //       repo: 'BasePipeline',
-    //       actionName: 'GitHub_Source',
-    //       connectionArn: 'arn:aws:codestar-connections:us-east-1:058632605534:connection/2d99428e-d740-40cb-9f88-ec8fd959dcf2'
-    //     }
-    //   )
-    // );
+    sourceStage.addAction(
+      new CodeStarConnectionsSourceAction(
+        {
+          output: sourceOutput,
+          branch: 'main',
+          owner: 'firepho92',
+          repo: 'BasePipeline',
+          actionName: 'GitHub_Source',
+          connectionArn: 'arn:aws:codestar-connections:us-east-1:058632605534:connection/2d99428e-d740-40cb-9f88-ec8fd959dcf2',
+          role:
+        }
+      )
+    );
 
-    // const buildStage = this.addStage({
-    //   stageName: 'Build',
-    // });
+    const buildStage = this.addStage({
+      stageName: 'Build',
+    });
 
-    // buildStage.addAction(
-    //   new CodeBuildAction(
-    //     {
-    //       actionName: 'CDK_Build',
-    //       project: new PipelineProject(this, 'CdkBuildProject'),
-    //       input: sourceOutput,
-    //       outputs: [cdkOutput],
-    //     }
-    //   )
-    // );
+    buildStage.addAction(
+      new CodeBuildAction(
+        {
+          actionName: 'CDK_Build',
+          project: new PipelineProject(this, 'CdkBuildProject'),
+          input: sourceOutput,
+          outputs: [cdkOutput],
+        }
+      )
+    );
   }
 }
